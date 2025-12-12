38820dcbc65d775310e79e5787624bc13b69db51
InnocentusLime <innocentuslime.help@gmail.com>
2025-12-12 23:17:40 +0200

    print bruh

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void OnFly(InputValue value)
+    {
+        Debug.Log("bruh");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
