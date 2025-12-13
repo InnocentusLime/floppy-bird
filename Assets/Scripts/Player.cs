cebccad9cdf224edd17fcc70c3d4e21372166a8e
InnocentusLime <innocentuslime.help@gmail.com>
2025-12-13 09:35:38 +0200

    simple falling

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float shellSize;
+
+    private Collider2D mCollider;
+    private Transform mTransform;
+    private RaycastHit2D[] hits;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mCollider = GetComponent<Collider2D>();
+        mTransform = mCollider.transform;
+        hits = new RaycastHit2D[3];
     }
 
     void OnFly(InputValue value)
@@ -18,6 +28,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int hitCount = mCollider.Cast(Vector2.down, hits, speed * Time.deltaTime);
+        float distance = speed * Time.deltaTime;
+        foreach (RaycastHit2D hit in hits.Take(hitCount))
+        {
+            distance = Mathf.Min(distance, hit.distance - shellSize);
+        }
+        mTransform.localPosition += (Vector3)(distance * Vector2.down);
+        if (hitCount != 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
