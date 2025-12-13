b123cfe435c8ab42f760b0f9ceda1ed98378edb7
InnocentusLime <innocentuslime.help@gmail.com>
2025-12-13 10:31:10 +0200

    basic mechanics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,13 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float acceleration;
     [SerializeField] private float shellSize;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float flySpeed;
+    [SerializeField] private float pull;
 
+    private float speed;
     private Collider2D mCollider;
     private Transform mTransform;
     private RaycastHit2D[] hits;
@@ -18,18 +22,26 @@ public class Player : MonoBehaviour
         mCollider = GetComponent<Collider2D>();
         mTransform = mCollider.transform;
         hits = new RaycastHit2D[3];
+        speed = 0.0f;
     }
 
     void OnFly(InputValue value)
     {
-        Debug.Log("bruh");
+        speed = -flySpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int hitCount = mCollider.Cast(Vector2.down, hits, speed * Time.deltaTime);
+        speed += acceleration * Time.deltaTime;
+        speed = Mathf.Min(speed, maxSpeed);
+        if (speed < 0.0) {
+            speed += pull * Time.deltaTime;
+        }
+
         float distance = speed * Time.deltaTime;
+
+        int hitCount = mCollider.Cast(Vector2.down, hits, distance);
         foreach (RaycastHit2D hit in hits.Take(hitCount))
         {
             distance = Mathf.Min(distance, hit.distance - shellSize);
@@ -37,6 +49,7 @@ public class Player : MonoBehaviour
         mTransform.localPosition += (Vector3)(distance * Vector2.down);
         if (hitCount != 0)
         {
+            speed = 0.0f;
             Destroy(gameObject);
         }
     }
